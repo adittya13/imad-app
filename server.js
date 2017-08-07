647759ad9b953d5f377bbb39878a69baffd7a81c
adittya13 <30629470+adittya13@users.noreply.github.com>
2017-08-07 20:18:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res) {
-   res.send('Article One requested and will be served here'); 
+app.get('/ui/article-one',function(req,res) {
+   res.sendfile(path.join(_dirname,'ui','aricle-one.html')); 
 });
 
-app.get('/article-two',function(req,res) {
-   res.send('Article two requested and will be served here'); 
+app.get('/ui/article-two',function(req,res) {
+   res.sendfile(path.join(_dirname,'ui','aricle-two.html')); 
 });
 
-app.get('/article-three',function(req,res) {
-   res.send('Article three requested and will be served here'); 
+app.get('/ui/article-three',function(req,res) {
+   res.sendfile(path.join(_dirname,'ui','aricle-three.html')); 
 });
 
 app.get('/ui/style.css', function (req, res) {
