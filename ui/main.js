76bb8954ac1701e0c1963d5318c195f27fea2670
adittya13 <30629470+adittya13@users.noreply.github.com>
2017-08-08 14:22:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var element = document.getElementById('main-text');
 element.innerHTML='This has been changed using JAVASCRIPT again!!';
 var img = document.getElementById('madi');
 img.onclick = function() {
-img.style.marginleft='400px';
+img.style.marginLeft='400px';
  }
