485aa82ec1929bb5cb17353fb3ca08d0a90fa4ca
adittya13 <30629470+adittya13@users.noreply.github.com>
2017-08-08 14:31:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,10 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='This has been changed using JAVASCRIPT again!!';
 var img = document.getElementById('madi');
+function moveRight() {
+    var marginLeft = marginLeft+10;
+    img.style.marginLeft= marginLeft+'px';
+} 
 img.onclick = function() {
-var interval=setInterval(moveLeft,100);
+var interval=setInterval(moveRight,30);
 };
